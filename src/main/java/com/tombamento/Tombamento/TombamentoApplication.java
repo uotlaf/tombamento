d3a1e4ca2f18d749d3cd2256f92d66edfae2bee9
uotlaf <git@uotlaf.com>
2026-08-26 14:35:53 -0300

    init new project

---
+++ src/main/java/com/tombamento/Tombamento/TombamentoApplication.java
@@ -0,0 +1,13 @@
+package com.tombamento.Tombamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TombamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TombamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tombamento/Tombamento/TombamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tombamento.Tombamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TombamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
